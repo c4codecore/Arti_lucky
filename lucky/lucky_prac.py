c04a3eb350f7f6ed8cf44d5dce757b88f1544c95
lks584752-ai <lks584752@gmail.com>
2025-09-03 03:43:05 +0000

    ji

--- lucky/lucky_prac.py
+++ lucky/lucky_prac.py
@@ -1,2 +1,23 @@
 print("hello World")
 print("ok")
+# print always take currenty value not old for exp.
+# a=10
+# a=20
+# print(a) # jaise es mei print value 20 lega instead of 10.
+
+# li=[10,20,30,40,60]
+# min=li[0]
+# for i in li:
+#    if i<min:
+#     min=i
+# print(min)
+# max=li[]
+li1=[1,2,3]
+li2=[4,5,6]
+length=len(li1)
+for i in range(length):
+    print(li1[i],li2[i])
+
+# for i in li1:
+#   if i in li2:
+#     print(i)
