c2f9b8d016542fb0db91cb4eb15943760a1b9981
DattenGurdeep <138423201+DattenGurdeep@users.noreply.github.com>
2025-09-03 03:42:09 +0000

    3

--- arti/artipython.py
+++ arti/artipython.py
@@ -1,2 +1,24 @@
-print("hello world")
-print("good morning sir ji")
+# print("hello world")
+# print("good morning sir ji")
+# list1=[10,20,30,40]
+# total=0
+# for i in list1:
+#     total+=i
+# avg=total/len(list1)
+# print(avg)
+# list1=[22,88,98,765,7642]
+# min=list1[0]
+# for i in list1:
+#     if min>i:
+#      min=i
+# print(min)
+
+list1=[1,2,3]
+list2=[4,5,6]
+length=len(list1)
+for i in range (length):
+    print(list1[i],list2[i])
+
+
+
+    
