af42b34a2afbe03c92debb8bb2e2fd255d3808f9
c4codecore <codecorecomputercenter@gmail.com>
2025-09-05 03:20:40 +0000

    second_largest_num

--- main.py
+++ main.py
@@ -31,12 +31,12 @@
 
 # vowels count
 
-li_1= [1, 2, 3]
-li_2= [4, 5, 6]
+# li_1= [1, 2, 3]
+# li_2= [4, 5, 6]
 
-length = len(li_2) # 3
+# length = len(li_2) # 3
 
-for i in range(length): # 0 1 2 
+# for i in range(length): # 0 1 2 
 
 # 1 4
 # 2 5
@@ -46,3 +46,18 @@ for i in range(length): # 0 1 2
 # 2) message--> commit --> sync
 
 
+li = [10,20,30,25,24,45]
+
+if li[0] > li[1]:
+    largest = li[0]
+    second_largest = li[1]
+else:
+    largest = li[1]
+    second_largest = li[0]
+
+for i in range(2, len(li)): 
+    if li[i] > largest:  
+        second_largest = largest
+        largest = li[i]
+    elif li[i] > second_largest:
+        second_largest = li[i]
