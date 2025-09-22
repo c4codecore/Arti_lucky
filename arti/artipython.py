2c7aabad6b020852579122a4ddb3e9739d30afb9
DattenGurdeep <138423201+DattenGurdeep@users.noreply.github.com>
2025-09-22 02:46:23 +0000

    while loop

--- arti/artipython.py
+++ arti/artipython.py
@@ -167,10 +167,10 @@
 # # print(total,count)
 # print(f"avg number 1 to 100 : {total/count}")
 
-i=5
-while i >=1: 
-    print(i)
-    i=i-1
+# i=5
+# while i >=1: 
+#     print(i)
+#     i=i-1
 
 # i=10
 # while i>=1:
@@ -194,15 +194,190 @@ while i >=1:
 #     i=i-1
 # print(f"the total of even number:{total}")
 
-i=10
-total=0
-count=0
-while i>=1:
-    if i%2==0:
-        total+=i
-        count+=1
-    i=i-1
-print(f"avg number of even :{total/count}")
+# i=10
+# total=0
+# count=0
+# while i>=1:
+#     if i%2==0:
+#         total+=i
+#         count+=1
+#     i=i-1
+# print(f"avg number of even :{total/count}")
+
+# a =""
+# while a != "exit":
+#         a = input("please enter something :")
+#         print(f"you have entered :{a}")
+# print("sucessfuly exit")
+
+# print("=======MENU=======")
+# print("A:greet")
+# print("B:show course list:")
+# print("C:show center details:")
+# print("D:contact information:")
+# print("E:exit")
+# choose=input("enter the option: ")
+# match choose:
+#     case "A":
+#         print("hello welcome to code core computer center")
+#     case "B":
+#         print("courses offerd")
+#         print("1:basic computer course")
+#         print("2:python programming")
+#         print("3:web desigining")
+#         print("4:data analytics")
+#     case "C":
+#         print("code core computer center:established in 2024,specialized in it courses")
+#     case "D":
+#         print("contact number:9013010909")
+#     case "E":
+#         print("thank you! goodbye")
+#     case _:
+#         print("invalid choice.please select between A to E.")
+
+# while True:
+#     print("=======MENU=======")
+#     print("A:greet")
+#     print("B:show course list:")
+#     print("C:show center details:")
+#     print("D:contact information:")
+#     print("E:exit")
+#     choose=input("enter the option: ")
+#     match choose:
+#         case "A":
+#             print("hello welcome to code core computer center")
+#         case "B":
+#             print("courses offerd")
+#             print("1:basic computer course")
+#             print("2:python programming")
+#             print("3:web desigining")
+#             print("4:data analytics")
+#         case "C":
+#             print("code core computer center:established in 2024,specialized in it courses")
+#         case "D":
+#             print("contact number:9013010909")
+#         case "E":
+#             print("thank you! goodbye")
+#             break
+#        case _:
+#            print("invalid choice.please select between A to E.")
+# exit=""
+# while exit!="exit":
+#     print("=======MENU=======")
+#     print("A:greet")
+#     print("B:show course list:")
+#     print("C:show center details:")
+#     print("D:contact information:")
+#     print("E:exit")
+#     choose=input("enter the option: ")
+#     match choose:
+        # case "A":
+        #     print("hello welcome to code core computer center")
+        # case "B":
+        #     print("courses offerd")
+        #     print("1:basic computer course")
+        #     print("2:python programming")
+        #     print("3:web desigining")
+        #     print("4:data analytics")
+        # case "C":
+        #     print("code core computer center:established in 2024,specialized in it courses")
+        # case "D":
+        #     print("contact number:9013010909")
+        # case "E":
+        #     print("thank you! goodbye")
+        #     exit="exit"
+        # case _:
+        #    print("invalid choice.please select between A to E.")
+
+# x=-1
+# if x:
+#     print("watter bootle")
+# else:
+#     print("coffee mug")
 
 
+# for i in range(3):
+#   print("for loop start",i)
+#   for j in range(5,7): 
+#        print("inside loop",i,j)
+#   print(i)
 
+# attempts=0
+# while attempts<3:
+#     passward=input("enter passward:")
+#     if passward=="python123":
+#         print("access granted!")
+#         break
+#     attempts+=1
+# else:
+#     print("too many failer attempts")
+
+# num=int(input("enter the number:"))
+# for i in range(2,num):
+#     if num%i==0:
+#         print("not prime number")
+#         break
+# else:
+#     print("prime number",num)
+
+
+# flag=True
+# num=int(input("enter the number:"))
+# for i in range(2,num):
+#     if num%i==0:
+#         flag=False
+#         break
+
+# # if flag:
+#     print(f"{num} is a prime number")
+# else:
+#     print(f"{num} is  not prime")
+
+# result=f"{num} is a prime number" if flag  else f"{num} is  not prime"
+# print(result)
+
+# i=0
+# num=int(input("enter the number:")) 
+# while num>=i: 
+#     print(i)
+#     i+=1
+
+
+# i=1
+# num=int(input("enter the number:"))
+# while num%2==0:
+#     print(f"{i}is a even number:")
+#     i+=1
+# # else:
+# #     print(f"{i}is a odd number:")
+
+# i=1
+# num=int(input("enter the number:")) #8
+# while num>=i: 
+#     if i%2: #1%2=1 true ,2%2 =0 false
+#         print(i)
+#     i+=1
+# n=int(input("enter the number:"))
+# for i in range(1,n+1):
+#    for j in range(1,n+1):
+#     print(n+1-j,end=" ")
+#    print() 
+ 
+# n=int(input("enter the number:"))
+# for row in range(1,n+1):
+#     for col in range(1,n+1):
+#         print(row,col,sep="",end=" ")
+#     print()
+
+# for i in range(2):
+#     for j in range(2):
+#         for k in range(2):
+#             print(i,j,k)
+
+# i=1
+# while i <=3:
+#     j=1
+#     while j<=3:
+#         print(i,j)
+#         j+=1
+#     i+=1
