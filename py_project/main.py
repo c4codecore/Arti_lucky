df14bcc9f2f2ac7dd034c624c115355c9bdacbfb
c4codecore <codecorecomputercenter@gmail.com>
2025-09-24 04:57:51 +0000

    email list updated

--- py_project/main.py
+++ py_project/main.py
@@ -2,25 +2,17 @@ import csv
 from send_email import send_email
 
 email_list = []
-with open("emails.csv", mode="r", encoding="utf-8") as file:
+with open("emails.csv", mode="r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        name = row["Name"]
-        email = row["Email"]
+        name = row["Name"].title()
+        email = row["Email"].lower()
         name_email = (name, email)
         email_list.append(name_email)
-
-formatted_email_list = []
-
-for name, email in email_list:
-    new_name = name.title()
-    new_email = email.lower()
-    new_tuple = (new_name, new_email)
-    formatted_email_list.append(new_tuple)
 
 
 with open('index.html', 'r') as file:
     html_body = file.read()
 
-print(f"Sending email to {len(formatted_email_list)} recipients...")
-send_email(html_body, formatted_email_list)
+print(f"Sending email to {len(email_list)} recipients...")
+send_email(html_body, email_list)
