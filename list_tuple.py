1733a7a5485be615f409ecbde90b8639568104d0
c4codecore <codecorecomputercenter@gmail.com>
2025-10-24 02:57:31 +0000

    24oct tuple

---
+++ list_tuple.py
@@ -0,0 +1,29 @@
+# tup = (10, 20, 30, 40)
+# item_to_remove = 30
+# li = list(tup) # [10, 20, 30, 40]
+# li.remove(item_to_remove) # [10, 20, 0]
+# tup = tuple(li) # (10, 20, 40)
+# print(tup) # (10, 20, 40)
+
+# tup = (10, 20, 30, 40)
+# item_to_find = 30
+# idx = tup.index(item_to_find)
+# print(idx)
+
+# tup = (10, 20, 30, 40)
+# item_to_find = 300
+# idx = tup.index(item_to_find)
+# print(idx)
+
+list_of_tuples = [(1, 2), (3, 4), (5, 6)]
+li0=[]  # [1,3,5]
+li1=[]  # [2,4,6]
+for ele in list_of_tuples: # (5,6)
+    li0.append(ele[0])
+    li1.append(ele[1])
+
+tup0= tuple(li0) # (1,3,5)
+tup1= tuple(li1) # (2,4,6)
+
+res = [tup0, tup1] # [(1,3,5), (2,4,6)]
+print(res)
--- main.py
+++ main.py
@@ -1,113 +1,113 @@
-# # li=[10,20,30,40]
-# # length = len(li) # 4
+# # # li=[10,20,30,40]
+# # # length = len(li) # 4
 
-# # for i in range(length-1, -1, -1): # 3 2 1 0
-# #     print(li[i])
+# # # for i in range(length-1, -1, -1): # 3 2 1 0
+# # #     print(li[i])
 
-# # for i in range(1, length+1): # 1 2 3 4
-# #     print(li[-i])
-
-
-# # -3 -2 -1 0 1 2 3 4 5 6
-
-# # range left se right means positively ---> end-1
-# # range right se left means negatively ---> end+1
+# # # for i in range(1, length+1): # 1 2 3 4
+# # #     print(li[-i])
 
 
+# # # -3 -2 -1 0 1 2 3 4 5 6
 
-# # 3 Sept 2025
-
-# # li = [100,20,150,125,200,15,150,99]
-# # max = li[0]
-
-# # for i in li:  
-# #     if i > max:  
-# #         max = i
-
-# # print(max)
+# # # range left se right means positively ---> end-1
+# # # range right se left means negatively ---> end+1
 
 
-# # Elephant
 
-# # vowels count
+# # # 3 Sept 2025
 
-# # li_1= [1, 2, 3]
-# # li_2= [4, 5, 6]
+# # # li = [100,20,150,125,200,15,150,99]
+# # # max = li[0]
 
-# # length = len(li_2) # 3
+# # # for i in li:  
+# # #     if i > max:  
+# # #         max = i
 
-# # for i in range(length): # 0 1 2 
-
-# # 1 4
-# # 2 5
-# # 3 6
-
-# # 1) git pull
-# # 2) message--> commit --> sync
+# # # print(max)
 
 
-# li = [10,20,30,25,24,45]
+# # # Elephant
 
-# if li[0] > li[1]:
-#     largest = li[0]
-#     second_largest = li[1]
-# else:
-#     largest = li[1]
-#     second_largest = li[0]
+# # # vowels count
 
-# for i in range(2, len(li)): 
-#     if li[i] > largest:  
-#         second_largest = largest
-#         largest = li[i]
-#     elif li[i] > second_largest:
-#         second_largest = li[i]
+# # # li_1= [1, 2, 3]
+# # # li_2= [4, 5, 6]
 
-# print("===== MENU =====")
-# print("1. Greet")
-# print("2. Show Course List")
-# print("3. Exit")
+# # # length = len(li_2) # 3
 
-# choice = int(input("Enter your choice (1-3): "))
+# # # for i in range(length): # 0 1 2 
 
-# match choice:
-#     case 1:
-#         print("Hello! Welcome to Code Core Computer Center")
-#     case 2:
-#         print("Courses Offered:")
-#         print("1. Basic Computer Course")
-#         print("2. Python Programming")
-#         print("3. Web Designing")
-#         print("4. Data Analytics")
-#     case 3:
-#         print("Thank you! Goodbye")
-#     case _:
-#         print("Invalid choice. Please select between 1 to 3.")
+# # # 1 4
+# # # 2 5
+# # # 3 6
 
-# name ="Arti"
-# name2="Lucky"
-# name3="Shivansh"
-# print(f"|{name}|")  # |Python    |
-# print(f"|{name2}|")  # |Python    |
-# print(f"|{name3}|")  # |Python    |
-
-laptop ="45000"
-charger= "1500"
-ppf="350"
-
-# print(f"|{"Laptop Price":<20}{":":^3}{laptop:>10}|") 
-# print(f"|{"Charger Price":<20}{":":^3}{charger:>10}|") 
-# print(f"|{"PPF Price":<20}{":":^3}{ppf:>10}|") 
-
-# |Laptop Price         :      45000|
-# |Charger Price        :       1500|
-# |PPF Price            :        350|
-
-# print(f"|{"Laptop Price":<20}{":":^3}{laptop:>10}|\n|{"Charger Price":<20}{":":^3}{charger:>10}|\n|{"PPF Price":<20}{":":^3}{ppf:>10}|") 
+# # # 1) git pull
+# # # 2) message--> commit --> sync
 
 
-print(f"|{"Laptop Price":<20}{":":^3}{laptop:0>5}|") 
-print(f"|{"Charger Price":<20}{":":^3}{charger:0>5}|") 
-print(f"|{"PPF Price":<20}{":":^3}{ppf:0>5}|") 
+# # li = [10,20,30,25,24,45]
+
+# # if li[0] > li[1]:
+# #     largest = li[0]
+# #     second_largest = li[1]
+# # else:
+# #     largest = li[1]
+# #     second_largest = li[0]
+
+# # for i in range(2, len(li)): 
+# #     if li[i] > largest:  
+# #         second_largest = largest
+# #         largest = li[i]
+# #     elif li[i] > second_largest:
+# #         second_largest = li[i]
+
+# # print("===== MENU =====")
+# # print("1. Greet")
+# # print("2. Show Course List")
+# # print("3. Exit")
+
+# # choice = int(input("Enter your choice (1-3): "))
+
+# # match choice:
+# #     case 1:
+# #         print("Hello! Welcome to Code Core Computer Center")
+# #     case 2:
+# #         print("Courses Offered:")
+# #         print("1. Basic Computer Course")
+# #         print("2. Python Programming")
+# #         print("3. Web Designing")
+# #         print("4. Data Analytics")
+# #     case 3:
+# #         print("Thank you! Goodbye")
+# #     case _:
+# #         print("Invalid choice. Please select between 1 to 3.")
+
+# # name ="Arti"
+# # name2="Lucky"
+# # name3="Shivansh"
+# # print(f"|{name}|")  # |Python    |
+# # print(f"|{name2}|")  # |Python    |
+# # print(f"|{name3}|")  # |Python    |
+
+# laptop ="45000"
+# charger= "1500"
+# ppf="350"
+
+# # print(f"|{"Laptop Price":<20}{":":^3}{laptop:>10}|") 
+# # print(f"|{"Charger Price":<20}{":":^3}{charger:>10}|") 
+# # print(f"|{"PPF Price":<20}{":":^3}{ppf:>10}|") 
+
+# # |Laptop Price         :      45000|
+# # |Charger Price        :       1500|
+# # |PPF Price            :        350|
+
+# # print(f"|{"Laptop Price":<20}{":":^3}{laptop:>10}|\n|{"Charger Price":<20}{":":^3}{charger:>10}|\n|{"PPF Price":<20}{":":^3}{ppf:>10}|") 
 
 
-res = ""
+# print(f"|{"Laptop Price":<20}{":":^3}{laptop:0>5}|") 
+# print(f"|{"Charger Price":<20}{":":^3}{charger:0>5}|") 
+# print(f"|{"PPF Price":<20}{":":^3}{ppf:0>5}|") 
+
+
+# res = ""
