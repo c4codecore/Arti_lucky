a27c0d184e87766a4fed5a2756afd505a67418a9
DattenGurdeep <138423201+DattenGurdeep@users.noreply.github.com>
2025-11-11 13:10:46 +0000

    7

---
+++ arti/functions.py
@@ -0,0 +1,61 @@
+# def is_even(num):
+#     if num%2==0:
+#         res="True"
+#     else:
+#         res="false"
+#     print(res)
+# list1=[10,20,40,68,79]
+# for i in list1:
+#     is_even(i)
+
+
+# def is_even(num):
+#     if num%2==0:
+#         res="True"
+#     else:
+#         res="false"
+#     print(res)
+# n=int(input("enter the number :"))
+# is_even(n)
+
+
+# def wish(name):
+#     print("hello",name,"good morning")
+# wish("coding center")
+# wish("python")
+
+# def sum_sub(a,b):
+#     sum=a+b
+#     sub=a-b
+#     return sum,sub
+# x,y=sum_sub(100,50)
+# print("the sum is:",x)
+# print("the sub is:",y)
+
+# def is_even(num):
+#     if num%2==0:
+#         res=True
+#     else:
+#         res=False
+#     return res
+# num=int(input("enter the number :"))
+# result=is_even(num)
+# print("this number is even",result)
+
+
+# def is_even(num):
+#     if num%2==0:
+#         res=True
+#     else:
+#         res=False
+#     return res
+# num=int(input("enter the number :"))
+# result=is_even(num)
+# if result:
+#     print("even")
+# else:
+#     print("odd")
+
+# list1=[10,30,59,60,20]
+# print(list1[2])
+
